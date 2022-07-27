019bb49325cd286a61e4d7ca993ea8635094cae6
Luyolo Mnganga <Luyolomnganga@gmail.com>
2022-07-27 16:21:41 +0200

    Initial commit

---
+++ config/footer.php
@@ -0,0 +1,12 @@
+<footer class="main-footer fixed-bottom">
+    <strong>Copyright &copy; <?php echo date('Y');?> 
+    <a href="#">Clinic Management System</a>.</strong> All rights reserved.
+    <div class="float-right d-sm-block">
+      Developed by WSU IT Students
+    </div>
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
